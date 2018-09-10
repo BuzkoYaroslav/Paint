2795b3a9c80c5ecd044f9126d83830142fa3c940
BuzkoYaroslav <yarik10.98@gmail.com>
2018-09-10 16:46:02 +0300

    create support files; minor changes

---
+++ Paint/Paint/Constants.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paint
+{
+    static class Constants
+    {
+        internal static class DrawingPanel
+        {
+            internal const int initialWidth = 200;
+            internal const int initialHeight = 200;
+
+            internal const string pictureFileDialogFilter = "Jpeg Image|*.jpg|Bitmap Image|*.bmp|Picture|*.png";
+            internal const string projectFileDialogFilter = "Paint project|*.paint";
+
+            internal const string openFileDialogTitle = "Открыть проект или рисунок...";
+            internal const string saveFileDialogTitle = "Сохранить текущий рисунок как...";
+
+            internal const string chooseInstrumentForDrawingInformationMessage = "Выберите инструмент для рисования!";
+
+            internal const string informationMessageTitle = "Info";
+            internal const string saveFileMessageBoxText = "Хотите ли сохранить проект?";
+            internal const string saveFileMessageBoxTitle = "Сохранение";
+        }
+    }
+}
---
+++ Paint/Paint/DrawingProjectManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using Paint.Geometric_framework;
+
+namespace Paint
+{
+    static class DrawingProjectManager
+    {
+        public static void SaveCanvasProject(Canvas canvas, string fileName)
+        {
+            var bf = new BinaryFormatter();
+
+            using (var fs = new FileStream(fileName, FileMode.OpenOrCreate))
+            {
+                bf.Serialize(fs, canvas);
+            }
+        }
+
+        public static Canvas LoadCanvasProject(string fileName)
+        {
+            var bf = new BinaryFormatter();
+
+            using (var fs = new FileStream(fileName, FileMode.Open))
+            {
+                return bf.Deserialize(fs) as Canvas;
+            }
+        }
+
+        public static void SaveCanvasPicture(Canvas canvas, string fileName)
+        {
+            canvas.CurrentState.Save(fileName);
+        }
+
+        public static Canvas OpenCanvasPicture(string fileName)
+        {
+            return new Canvas(Image.FromFile(fileName));
+        }
+    }
+}
